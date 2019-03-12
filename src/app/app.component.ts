01b0d32b05669d1e6c4a376121702e0eb1391eba
VDC\qtran31 <qtran31@csc.com>
2019-03-12 09:07:41 +0700

    q7-002: Practice directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-userName: string = '';
-onResetUserName() {
-  this.userName = '';
-}
+export class AppComponent implements OnInit {
+  private logs: string[] = [];
+  // tslint:disable-next-line:no-inferrable-types
+  private isShowDetail: boolean = false;
+  ngOnInit() {}
+  onToggle() {
+    this.isShowDetail = ! this.isShowDetail;
+    const {length} = this.logs;
+    this.logs.push(`log ${length}`);
+  }
 }
